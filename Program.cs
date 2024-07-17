52c2f1dccb9e32013bd90b194486da89c9f6d4b9
Вера <veraydina123@gmail.com>
2024-07-17 21:00:13 +0300

    Задача2,3

--- Program.cs
+++ Program.cs
@@ -7,18 +7,62 @@
 
    
     
-int M = 10;
-int N = 20; 
+// int M = 10;
+// int N = 20; 
 
- PrintNumbers (M, N);
+//  PrintNumbers (M, N);
     
 
-void PrintNumbers(int currentNumber, int endNumber)
+// void PrintNumbers(int currentNumber, int endNumber)
+//     {
+//         if (currentNumber <= endNumber)
+//         {
+//             Console.Write(currentNumber+",");
+//             PrintNumbers(currentNumber + 1, endNumber); 
+//         }
+//     }
+
+
+
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+//    long Ackermann(long m, long n)
+//     {
+//         if (m == 0) return n + 1; 
+//         if (n == 0) return Ackermann(m - 1, 1); 
+//         return Ackermann(m - 1, Ackermann(m, n - 1)); 
+//     }
+
+     
+//     {
+//         long m = 3, n = 4; 
+//         long result = Ackermann(m, n);
+//         Console.WriteLine($"A({m}, {n}) = {result}");
+//      }
+
+
+
+// Задача 3: Задайте произвольный массив. Выведете его элементы,
+//  начиная с конца. Использовать рекурсию, не использовать циклы.
+     
+
+
+
+
+
+   
+    
+        int[] d = new int[] { 1, 2, 3, 4, 5 }; 
+
+        PrintArrayReverse(d);
+    
+
+    static void PrintArrayReverse(int[] array)
     {
-        if (currentNumber <= endNumber)
+        if (array.Length > 0)
         {
-            Console.Write(currentNumber+",");
-            PrintNumbers(currentNumber + 1, endNumber); // Рекурсивный вызов
+            Console.Write($"{array[array.Length - 1]} ");
+            PrintArrayReverse(array.Take(array.Length - 1).ToArray()); 
         }
     }
-
