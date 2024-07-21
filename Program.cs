c113a4f705ed456e461e8ac8b9e8644ca213de42
Вера <veraydina123@gmail.com>
2024-07-21 19:10:55 +0300

    Решение задач

--- Program.cs
+++ Program.cs
@@ -166,7 +166,7 @@
 //    }
 //    return false;
 // }
- 
+
 
 //  System.Console.WriteLine("Input size of array: ");
 //  int size = Convert.ToInt32(Console.ReadLine());
@@ -175,7 +175,7 @@
 //  System.Console.WriteLine("Input maximal value of array element: ");
 //  int max = Convert.ToInt32(Console.ReadLine());
 
- 
+
 
 //  int[] myArray = CreateRandomArray(size, min, max);
 //  PrintArray(myArray);
@@ -183,3 +183,69 @@
 //  System.Console.WriteLine("Input number: ");
 //  int num = Convert.ToInt32(Console.ReadLine());
 // System.Console.WriteLine(SearchNum(myArray, num));
+
+
+//Задайте массив из 10 элементов, заполненный числами из промежутка
+//[-10, 10]. Замените отрицательные элементы на положительные,
+// а положительные на отрицательные.
+// Пример 
+// [1 -5 6]
+// => [-1 5 -6]
+
+// int[] CreateRandomArray(int size, int min, int max)
+// {
+//     int[]array = new int[size];
+//     Random random = new Random();
+//     for(int i = 0; i < array.Length;i++)
+//     {
+//         array[i] = random.Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[]array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] +" ");
+//     }
+//     System.Console.WriteLine();
+// }
+// int[] HangeArray(int[] array)
+// {
+//   for(int i = 0; i < array.Length; i++) 
+//     {       
+//         array[i] *= -1;        
+//     }
+//    return array;
+// }        
+
+
+
+//  System.Console.WriteLine("Input size of array: ");
+//  int size = Convert.ToInt32(Console.ReadLine());
+//  System.Console.WriteLine("Input minimal value of array element: ");
+//  int min = Convert.ToInt32(Console.ReadLine());
+//  System.Console.WriteLine("Input maximal value of array element: ");
+//  int max = Convert.ToInt32(Console.ReadLine());
+
+
+
+//  int[] myArray = CreateRandomArray(size, min, max);
+//  PrintArray(myArray);
+//  myArray = HangeArray(myArray);
+//  PrintArray(myArray);
+
+
+
+
+// Найдите произведение пар чисел в одномерном массиве.Парой считается
+// первый и последний элемент,второй и предпоследний и т.д.
+// Результат записать в новый массив.
+
+   
+        
+ 
+
+
+ 
