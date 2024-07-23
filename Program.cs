ecaa22bcab88034e0521fe17219a9bcfca3c256d
Вера <veraydina123@gmail.com>
2024-07-23 22:03:32 +0300

    Задача из раздела функции

--- Program.cs
+++ Program.cs
@@ -266,3 +266,45 @@
 //  int num = Convert.ToInt32(Console.ReadLine());
  
 //  System.Console.WriteLine(DigitCounter(num));
+
+// Задайте массив из N случайных целых чисел
+//  (N вводится с клавиатуры).
+//  Найдите колличество чисел, которое оканчивается на 1 
+//   и делится нацело на 7.
+//   Пример:
+//   [1 5 11 21 81 4 0 91 2 3] => 2
+
+// Cоздали массив и заполнили его значениями с клавиатуры.
+
+
+int[] InputArray(int size)
+{
+    int[] arr = new int [size];
+    
+    for(int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"Input {i+1} number: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+
+int ArrayCounter (int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 10 == 1 && arr[i] % 7 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine()); 
+
+int[] arr = InputArray(size);
+System.Console.WriteLine(ArrayCounter (arr));
