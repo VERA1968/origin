ff4f1c12b4d6d024c2f8375ee280f23b15cfa5e5
Вера <veraydina123@gmail.com>
2024-08-19 19:30:07 +0300

    Добавили задачу

--- Program.cs
+++ Program.cs
@@ -498,23 +498,11 @@
 //  string s2 = new String(ch_array);
 
 //  string s3 = new string('a', 6); // Результатом будет "аааааа"
+  
 
-//    string CreateString(string s) 
-//    {
-//         string Letters = "";
-//         foreach (char e in s)
-//         {
-//             if( char.IsAsciiLetter(e))
-//             {
-//                 Letters = Letters + e;
-//             }
-//         }
-//         return Letters;
-//    }
 
-//     string str = Console.ReadLine();
-//     string result = CreateString(str); 
-//     Console.WriteLine(result);
+
+    
 
 
 
@@ -609,24 +597,57 @@
   
     
        
-string[] inputArray = new string[] { "aa", "bbbb", "c", "dddd", "e", "ff", "ggg", "hhhh", "iii", "jjj" };
+// string[] inputArray = new string[] { "aa", "bbbb", "c", "dddd", "e", "ff", "ggg", "hhhh", "iii", "jjj" };
 
-string[] shortStrings = new string[0];
+// string[] shortStrings = new string[0];
        
-for (int i = 0; i < inputArray.Length; i++)
-    {
-        if (inputArray[i].Length <= 3)
-        {
-            string[] temp = new string[shortStrings.Length + 1];
-            Array.Copy(shortStrings, temp, shortStrings.Length);
-            temp[shortStrings.Length] = inputArray[i];
-            shortStrings = temp;
-        }
-    }
+// for (int i = 0; i < inputArray.Length; i++)
+//     {
+//         if (inputArray[i].Length <= 3)
+//         {
+//             string[] temp = new string[shortStrings.Length + 1];
+//             Array.Copy(shortStrings, temp, shortStrings.Length);
+//             temp[shortStrings.Length] = inputArray[i];
+//             shortStrings = temp;
+//         }
+//     }
 
        
-foreach (string s in shortStrings)
-{
-    Console.Write(s + " , ");
-}
+// foreach (string s in shortStrings)
+// {
+//     Console.Write(s + " , ");
+// }
    
+
+// ЗАПОЛНЕНИЕ И ВЫВОД ДВУМЕРНОГО МАССИВА С ПРИМЕНЕНИЕ ФУНКЦИИ.
+
+using System;
+class Program
+{
+static void FindMaxInRows(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        int max = array[i, 0];
+        for (int j = 1; j < cols; j++)
+        {
+            if (array[i, j] > max)
+        {
+        max = array[i, j];
+        }
+        }
+            Console.WriteLine($"Максимальный элемент в строке {i}:{max}");
+    }
+}
+static void Main()
+{
+    int[,] array =
+    {
+    { 1, 3, 5, 7 },
+    { 2, 4, 6, 8 },
+    { 9, 10, 11, 12 }}   ;
+    FindMaxInRows(array);
+    }
+}
